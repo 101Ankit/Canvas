17e04ff8e9a56acc0ba2ec5e92ed6b2994637cd5
Ankit Joshi <ankit101joshi@gmail.com>
2018-01-27 00:58:02 +0530

    Finally new color theme added

--- Canvas.js
+++ Canvas.js
@@ -2,48 +2,28 @@ var Canvas = document.querySelector('Canvas');
 Canvas.width = window.innerWidth;
 Canvas.height = window.innerHeight;
 var c = Canvas.getContext('2d'); //Here this c variable wil stand for context;
-/*
-c.fillStyle = 'rgba(255,255,0,0.5)';
-c.fillRect(100, 100, 100,100);
-c.fillRect(200, 100, 100,100);//c.fillRect(x, y, width,height)
-c.fillRect(300, 100, 100, 100);
-c.fillRect(300,200,100,100);
-c.fillRect(300, 300, 100,100);
-c.fillRect(300, 400, 100,100);
-c.fillRect(100, 200, 100,100);
-c.fillRect(100, 300, 100,100);
-c.fillRect(100, 400, 100,100);
-c.fillRect(200, 300, 100,100);
-console.log(Canvas); //Made A ;
-//Line
-c.beginPath();
-c.moveTo(50,300);//c.moveTo(x,y) // Going to a point, This will be invisible unless we call the stroke method
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle= "#fa34a3";
-c.stroke();
-//Arc / Circle
-/* syntax /here angles are in radian
-c.arc(x:Int, y: Int, r: Int, 
-	startAngle:Float, EndAngle:Float
-	drawCounterClockwise: Bool (false));
-*/
-
-/*
-//multiple-circles using for loop
-for(let i=0; i< 1000; i++)
-{
-	var x= Math.random() * window.innerWidth;
-    var y= Math. random() * window.innerHeight;
-	c.beginPath()
-    c.arc(x, y,30, 0, Math.PI * 2,false);
-    c.strokeStyle=    'rgb(' + (Math.floor(Math.random() * 256)) + ',' 
-    	+ (Math.floor(Math.random() * 256)) + ',' 
-    	+ (Math.floor(Math.random() * 256)) + ')';
-    c.stroke();
+var mouse = {
+	x: undefined,
+	y:undefined,
 }
-*/
-
+var maxRadius = 40;
+//var minRadius = 2;
+var colorArray =[
+'#292C44',
+'#FF5349',
+'#F0F0F1',
+'#18CDCA',
+'#4F80E1',
+];
+window.addEventListener('mousemove', function(event){
+	mouse.x=event.x;
+	mouse.y=event.y;
+})
+window.addEventListener('resize', function(resize){
+	Canvas.width = window.innerWidth;
+Canvas.height = window.innerHeight;
+init();
+})
 function Circle(x,y,dx,dy,radius)
 {
 	this.x=x;
@@ -51,11 +31,12 @@ function Circle(x,y,dx,dy,radius)
 	this.dx=dx;
 	this.dy=dy;
 	this.radius=radius;
+	this.minRadius = radius;
+	this.color=colorArray[Math.floor(Math.random() * colorArray.length)]
 	this.draw = function(){
       c.beginPath()
 c.arc(this.x, this.y,this.radius, 0, Math.PI * 2,false);
-c.strokeStyle='blue';
-c.stroke();
+c.fillStyle = this.color;
 c.fill();
 	}
  this.update = function(){
@@ -69,14 +50,28 @@ if(this.y + this.radius > innerHeight || this.y - this.radius < 0)
 }
 this.x+= this.dx;
 this.y+=this.dy;
+//interactivity
+if(mouse.x - this.x < 50 && mouse.x -this.x >-50 
+&& mouse.y - this.y< 50 && mouse.y -this.y > -50 ) {
+if(this.radius < maxRadius) {
+	this.radius += 1;
+}
+} 
+
+else if(this.radius > this.minRadius) {
+this.radius -= 1;
+}
+
 this.draw();
  }
 }
-
-   var circleArray=[];
- for(var i=0;i<100;i++)
+var circleArray=[];
+function init()
+{
+	 circleArray=[];
+ for(var i=0;i<900;i++)
  {
- 	var radius=30;
+ 	var radius=Math.random() * 3 + 1;
  	var x=Math.random() * (innerWidth - radius * 2) + radius; //substract the diameter of the circle from the innerWidth of the circle so that it does'nt gets caught up in right side of the screen and add radius so that the same does'nt happen on the other side;
 	var y=Math.random() * (innerHeight -radius * 2) + radius; 
     var dx = Math.random()- 0.5 ;//velocity of x;
@@ -84,9 +79,8 @@ this.draw();
     
  	circleArray.push(new Circle(x,y,dx,dy,radius));
  }
+}	
 
-
-	
 function animate(){
 
 	requestAnimationFrame(animate);
@@ -96,5 +90,6 @@ for(var i=0; i < circleArray.length;i++)
 	circleArray[i].update();
 }
 }
+init();
 animate();
 
